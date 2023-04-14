87e3098b82dfdc7946d4664c9e77016e98650afc
kevinegemba <kegemba@fandm.edu>
2023-04-14 15:37:40 -0400

    Update Clean Code.R

--- Clean Code.R
+++ Clean Code.R
@@ -20,7 +20,7 @@ latitude <- status_intensity_observation$Latitude[match(plant_id,status_intensit
 longitude <- status_intensity_observation$Longitude[match(plant_id,status_intensity_observation$Individual_ID)]
 plant_data_id <- data.frame(plant_id,latitude,longitude)
 
-names(plant_data_id)[1] <- "Individual_ID"
+names(plant_data_id)[1] <- "id"
 
 #subsettig phenology dataet
 status_intensity_observation_sub  <- status_intensity_observation[,c(4,5,13,15,16,18)]
@@ -38,7 +38,9 @@ stations <- meteo_nearby_stations(plant_data_id,
                                   limit = 1)
 
 # converts from list to tibble (data.frame)
-stations <- do.call(rbind.data.frame, stations)
+stations2 <- do.call(rbind.data.frame, stations)
+stations2$plant_id <- names(stations)
+stat_plant <- stations2
 
 #Saving Dataframe stations
 write.csv(stations, file = '/Users/kegem/Desktop/GitHub/Project13/Milkweed-phenology/stations_csv', row.names = FALSE)
@@ -72,10 +74,26 @@ write.csv(met_data, file = '/Users/kegem/Desktop/GitHub/Project13/Milkweed-pheno
 #uploading stations data
 met_data <- read.csv("met_data_csv", header = TRUE)
 
+names(plant_data_id)[1] <- "stations.id"
 
 
 # join met_data with plant_data
+plant_meteo_data <- merge(plant_data_id, met_data, by = "stations.id")
+
 stat_plant <- data.frame(plant_data_id,stations$id)
+
+
+
+names(stat_plant)[1] <- "id"
+
+
+# merge the stat_plant data with met_data by id
+plant_meteo_data <- merge(stat_plant, met_data[,c("stations.id","date","tmax","prcp")], by = "stations.id")
+
+
+
+# join stat_mean data with status_intesnity_observation_sub
+plant_meteo_stations_data <- inner_join(plant_meteo_data,status_intensity_observation_sub, by="id")
 
 #calcuate means by station and year
 
@@ -85,8 +103,18 @@ met_data$Year <- substr(met_data$date, 1, 4)
 tapply(met_data$tmax,met_data[,c("id","Year")],mean,na.rm=T)
 
 stat_mean <- met_data %>%
-  group_by("year","id") %>%
+  
+  group_by(Year,id) %>%
   summarize(mean_precip = mean(prcp, na.rm = TRUE),
             mean_tmax = mean(tmax, na.rm = TRUE))
 
-plant
+#change Individual_ID to id
+names(status_intensity_observation_sub)[3] <- "id"
+status_intensity_observation_sub$id <- as.integer(status_intensity_observation_sub$id)
+stat_mean$id <- as.integer(stat_mean$id)
+
+# join stat_mean data with status_intesnity_observation_sub
+plant_meteo_data <- inner_join(stat_mean,status_intensity_observation_sub,by="stations.id")
+
+merged_data <- merge(status_intensity_observation_sub, plant_meteo_data[c("id", "tmax", "prcp")], by = "id")
+
